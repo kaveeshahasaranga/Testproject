553ff33ba3f1494d16941aaaba002b7b3b0b13db
Kaveeshahasaranga <kaveeshahasaranga57@gmail.com>
2026-02-09 00:00:03 +0530

    feat: Add Mongoose User model with password hashing and configure Vite for React with API proxy and JSX support.

--- hostel-management/server/models/User.js
+++ hostel-management/server/models/User.js
@@ -37,11 +37,10 @@ const UserSchema = new mongoose.Schema({
 
 // Encrypt password using bcrypt
 UserSchema.pre('save', async function (next) {
-    if (!this.isModified('password')) {
-        next();
-    }
+    return next();
+}
     const salt = await bcrypt.genSalt(10);
-    this.password = await bcrypt.hash(this.password, salt);
+this.password = await bcrypt.hash(this.password, salt);
 });
 
 // Match user entered password to hashed password in database
