b70ebff8ec61dc42c6b22a0fb3e51f02977bcd81
kaveeshahasaranga <kaveeshahasaranga57@gmail.com>
2026-03-26 19:10:14 +0530

    Add file type and size validation to upload middleware.

--- hostel-management/server/middleware/uploadMiddleware.js
+++ hostel-management/server/middleware/uploadMiddleware.js
@@ -10,6 +10,25 @@ const storage = multer.diskStorage({
 });
 
 // Check file type
+function checkFileType(file, cb) {
+    const filetypes = /jpeg|jpg|png|gif/;
+    const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
+    const mimetype = filetypes.test(file.mimetype);
 
+    if (mimetype && extname) {
+        return cb(null, true);
+    } else {
+        cb('Error: Images Only!');
+    }
+}
+
+// Init upload
+const upload = multer({
+    storage: storage,
+    limits: { fileSize: 5000000 }, // 5MB limit
+    fileFilter: function (req, file, cb) {
+        checkFileType(file, cb);
+    }
+});
 
 module.exports = upload;
