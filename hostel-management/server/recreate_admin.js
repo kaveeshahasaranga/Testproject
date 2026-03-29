37af35d19622b6a58a11862274837915facd81f5
kaveeshahasaranga <kaveeshahasaranga57@gmail.com>
2026-03-29 23:21:06 +0530

    chore: remove admin recreation logic from recreate_admin script

--- hostel-management/server/recreate_admin.js
+++ hostel-management/server/recreate_admin.js
@@ -10,32 +10,4 @@ const recreateAdmin = async () => {
         await mongoose.connect(process.env.MONGO_URI);
         console.log('MongoDB Connected...');
 
-        const email = 'admin@hostel.com';
-        const password = 'password123';
-
-        // 1. Delete existing admin
-        const deleted = await User.deleteOne({ email });
-        console.log(`Deleted existing admin: ${deleted.deletedCount}`);
-
-        // 2. Create new admin
-        const admin = await User.create({
-            name: 'System Admin',
-            itNumber: 'ADMIN002', // Changed to ensure uniqueness if ADMIN001 is stuck
-            email: email,
-            password: password,
-            role: 'admin'
-        });
-
-        console.log('Admin user recreated successfully.');
-        console.log('Email:', admin.email);
-        console.log('Role:', admin.role);
-        console.log('Password Hash:', admin.password); // Should look like a bcrypt hash
-
-        process.exit();
-    } catch (err) {
-        console.error(err);
-        process.exit(1);
-    }
-};
-
-recreateAdmin();
+        
